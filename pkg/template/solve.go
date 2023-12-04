e64b65ef91df6166b0d2903724f74f0ae615f93d
Linus Back <linus.a.back@gmail.com>
2023-12-04 18:08:58 +0100

    add template

---
+++ pkg/template/solve.go
@@ -0,0 +1,26 @@
+package template
+
+import (
+	"advent-of-code-2023/pkg/util"
+	"embed"
+)
+
+//go:embed *.txt
+var f embed.FS
+
+func Solve() (err error) {
+	var (
+		b []byte
+	)
+	b, err = f.ReadFile("example.txt")
+	if err != nil {
+		return
+	}
+
+	err = util.DoEachRow(b, func(row []byte, nr int) error {
+
+		return nil
+	})
+
+	return
+}

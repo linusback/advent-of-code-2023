ed258ef26412218ba119b4641ea756c8009105b2
Linus Back <linus.a.back@gmail.com>
2023-12-08 13:50:55 +0100

    prep day 9

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"advent-of-code-2023/pkg/day8"
+	"advent-of-code-2023/pkg/day9"
 	"fmt"
 	"time"
 )
@@ -15,5 +15,5 @@ func timeFunction(function func() error) {
 	fmt.Println("Time elapsed:", time.Since(start))
 }
 func main() {
-	timeFunction(day8.Solve)
+	timeFunction(day9.Solve)
 }
---
+++ pkg/day9/solve.go
@@ -0,0 +1,29 @@
+package day9
+
+import (
+	"advent-of-code-2023/pkg/util"
+	"embed"
+)
+
+//go:embed *.txt
+var f embed.FS
+
+func Solve() (err error) {
+	var (
+		b []byte
+	)
+	b, err = f.ReadFile("example.txt")
+	if err != nil {
+		return
+	}
+
+	err = util.DoEachRow(b, func(row []byte, nr int) error {
+
+		return nil
+	})
+	if err != nil {
+		return
+	}
+
+	return
+}

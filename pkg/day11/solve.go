38d221291bc7a265197266570c25df556616943d
Linus Back <linus.a.back@gmail.com>
2023-12-11 15:21:15 +0100

    day 11 done

--- pkg/day11/solve.go
+++ pkg/day11/solve.go
@@ -5,6 +5,7 @@ import (
 	"embed"
 	"fmt"
 	"strconv"
+	"time"
 )
 
 //go:embed *.txt
@@ -17,44 +18,26 @@ type point struct {
 func Solve() (err error) {
 	var (
 		b   []byte
-		b3  [][]byte
 		gal []point
 	)
 	b, err = f.ReadFile("input.txt")
 	if err != nil {
 		return
 	}
+	start := time.Now()
 
-	b2 := bytes.Split(b, []byte{'\n'})
-	columns := make([]int, len(b2[0]))
+	tiles := bytes.Split(b, []byte{'\n'})
+	rows := make([]int, len(tiles[0]))
+	columns := make([]int, len(tiles[0]))
 	galaxies := 0
-	//printMap(b2)
-	for y := 0; y < len(b2); y++ {
+	fmt.Printf("setup %v\n", time.Since(start))
+	start = time.Now()
+	for y := 0; y < len(tiles); y++ {
 		galaxies = 0
-		b3 = append(b3, b2[y])
-		for x := 0; x < len(b2[y]); x++ {
-			if b2[y][x] == '#' {
+		for x := 0; x < len(tiles[y]); x++ {
+			if tiles[y][x] == '#' {
 				galaxies++
 				columns[x]++
-			}
-		}
-		if galaxies == 0 {
-			b3 = append(b3, b2[y])
-		}
-	}
-	i := 0
-	//fmt.Println(columns)
-	for x := 0; x < len(columns); x++ {
-		if columns[x] > 0 {
-			continue
-		}
-		//fmt.Println("create colum")
-		createColumn(b3, x, i)
-		i++
-	}
-	for y, row := range b3 {
-		for x, c := range row {
-			if c == '#' {
 				gal = append(gal, point{
 					x:    x,
 					y:    y,
@@ -62,42 +45,90 @@ func Solve() (err error) {
 				})
 			}
 		}
+		rows[y] = galaxies
 	}
+	for x := 0; x < len(columns); x++ {
+		if columns[x] > 0 {
+			columns[x] = 0
+			continue
+		}
+		columns[x] = 1
+	}
+	for y := 0; y < len(rows); y++ {
+		if rows[y] > 0 {
+			rows[y] = 0
+			continue
+		}
+		rows[y] = 1
+	}
+	fmt.Printf("parsing %v\n", time.Since(start))
+	start = time.Now()
 
-	pairs := findPairs(gal)
+	//pairs := findPairs(gal)
+	//fmt.Printf("find pairs %v\n", time.Since(start))
+	//fmt.Println(len(pairs))
+	//start = time.Now()
+
+	//fmt.Println(rows)
+	//fmt.Println(columns)
 	//printMap(b3)
 	//printMapWithPoints(b3, gal)
-	result1 := sumDistance(pairs)
+	result1, result2 := sumDistance(gal, rows, columns)
+	fmt.Printf("find result %v\n", time.Since(start))
 	fmt.Println(result1)
+	fmt.Println(result2)
 	return
 }
 
-func sumDistance(pairs [][]point) int {
-	var res int
-	for _, pair := range pairs {
-		res += sumDistanceBetweenPoints(pair)
+func sumDistance(arr []point, rows, column []int) (res1 int, res2 int) {
+	var r1, r2, x1, x2, y1, y2 int
+	for i := 0; i < len(arr); i++ {
+		for k := i + 1; k < len(arr); k++ {
+			x1 = arr[i].x
+			x2 = arr[k].x
+			y1 = arr[i].y
+			y2 = arr[k].y
+			if arr[i].x > arr[k].x {
+				x1, x2 = x2, x1
+			}
+			if arr[i].y > arr[k].y {
+				y1, y2 = y2, y1
+			}
+			r1, r2 = sumDistanceBetweenPoints(x1, x2, y1, y2, rows, column)
+			res1 += r1
+			res2 += r2
+		}
 	}
-	return res
+	return res1, res2
 }
 
-func sumDistanceBetweenPoints(p []point) int {
-	var x, y int
-	if p[0].x < p[1].x {
-		x = p[1].x - p[0].x
-	} else {
-		x = p[0].x - p[1].x
-	}
+const part2Mul = 1_000_000 - 1
 
-	if p[0].y < p[1].y {
-		y = p[1].y - p[0].y
-	} else {
-		y = p[0].y - p[1].y
+//const part2Mul = 99
+
+func sumDistanceBetweenPoints(x1, x2, y1, y2 int, rows, column []int) (int, int) {
+	var part1, part2, tmp int
+	part1 = getExpanseBetween(x1, x2, column)
+	part2 = part1 * part2Mul
+
+	tmp = getExpanseBetween(y1, y2, rows)
+	part1 += tmp
+	part2 += tmp * part2Mul
+
+	tmp = x2 - x1 + y2 - y1
+	//fmt.Printf("len between %d and %d is %d\n", p[0].name, p[1].name, x+y+part1)
+	return tmp + part1, tmp + part2
+}
+
+func getExpanseBetween(from, to int, list []int) (res int) {
+	for i := from + 1; i < to; i++ {
+		res += list[i]
 	}
-	//fmt.Printf("len between %d and %d is %d\n", p[0].name, p[1].name, x+y)
-	return x + y
+	return
 }
 
 func findPairs[A ~[]K, K any](arr A) (res []A) {
+	res = make([]A, 0, 96141)
 	for i := 0; i < len(arr); i++ {
 		for k := i + 1; k < len(arr); k++ {
 			e := make([]K, 2)

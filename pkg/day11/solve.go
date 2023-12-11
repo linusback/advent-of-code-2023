a44710cd4aaf3e86e131af8118b1d6ff8886c5ae
Linus Back <linus.a.back@gmail.com>
2023-12-11 23:26:41 +0100

    improved day11

--- pkg/day11/solve.go
+++ pkg/day11/solve.go
@@ -9,84 +9,67 @@ import (
 //go:embed *.txt
 var f embed.FS
 
-type point struct {
-	x, y         uint64
-	xExp, yExp   uint64
-	xExp2, yExp2 uint64
-}
-
 const (
-	galaxyByte byte   = '#'
-	part2Mul   uint64 = 1_000_000 - 1
-	//part2Mul uint64 = 99
+	galaxyByte     byte   = '#'
+	part2Mul       uint64 = 1_000_000 - 1
+	rowLen, colLen        = 140, 140
 )
 
 func Solve() (res1, res2 int64, err error) {
 	var (
 		fil      fs.File
-		i        int
 		galaxies uint8
+		buff     [colLen + 1]byte
+		columns  [colLen]uint8
 
-		rowE, res1t, res2t, x, prev1, prev2, mul, e1, e2 uint64
+		expanse, res1t, res2t, x, prev1, prev2, mul, e1, e2 uint64
 	)
+
 	fil, err = f.Open("input.txt")
 	if err != nil {
 		return
 	}
-	tiles := [140][141]byte{}
-	columns := [140]uint8{}
-	//tiles := [10][10]byte{}
-	//columns := [10]uint8{}
 
-	//buff := make([]byte, 141)
-	//buff := make([]byte, 11)
-
-	for ; i < len(tiles); i++ {
-		_, err = fil.Read(tiles[i][:])
+	for y := uint64(0); y < rowLen; y++ {
+		_, err = fil.Read(buff[:])
 		if err != nil && err != io.EOF {
 			return
 		}
-		if err == io.EOF {
-			break
-		}
-	}
-
-	colGal := make([]uint8, len(tiles[0]))
-
-	for y := uint64(0); y < uint64(len(tiles)); y++ {
 		galaxies = 0
-		for x = 0; x < uint64(len(tiles[y])-1); x++ {
-			if tiles[y][x] == galaxyByte {
+		for x = 0; x < colLen; x++ {
+			if buff[x] == galaxyByte {
 				columns[x]++
 				galaxies++
-				e1 = y + rowE
-				e2 = y + rowE*part2Mul
-				colGal[x]++
+				e1 = y + expanse
+				e2 = y + expanse*part2Mul
 
 				res1t, res2t, prev1, prev2 = calculate(e1, e2, res1t, res2t, prev1, prev2, mul)
 				mul++
 			}
 		}
 		if galaxies == 0 {
-			rowE++
+			expanse++
+		}
+		if err == io.EOF {
+			break
 		}
 	}
 
 	prev1, prev2 = 0, 0
 	x = 0
 	mul = 0
-	rowE = 0
-	for ; x < uint64(len(columns)); x++ {
+	expanse = 0
+	for ; x < colLen; x++ {
 		if columns[x] > 0 {
-			for galaxies = 0; galaxies < colGal[x]; galaxies++ {
-				e1 = x + rowE
-				e2 = x + rowE*part2Mul
+			for galaxies = 0; galaxies < columns[x]; galaxies++ {
+				e1 = x + expanse
+				e2 = x + expanse*part2Mul
 				res1t, res2t, prev1, prev2 = calculate(e1, e2, res1t, res2t, prev1, prev2, mul)
 				mul++
 			}
 			continue
 		}
-		rowE++
+		expanse++
 	}
 
 	res1, res2 = int64(res1t), int64(res2t)
